bf61acb1407763cedc6ba09461f6932361f1627d
Vegard Lysne <Vegard.Lysne@uib.no>
2019-02-27 13:15:27 +0100

    Added an ID_var argument to the format_normalize function. Fixed the bug where grand baseline-mean was subtracted in stead of individual baselinemeans.

--- R/dataformat.R
+++ R/dataformat.R
@@ -4,6 +4,7 @@
 #' mean of the baseline period
 #'
 #' @param .data a tibble
+#' @param ID_var the ID variable, for grouping
 #' @param variable name of the variable that is going to be transformed
 #' @param phase_var name of the variable containing experimental phases, defaults to Experiment_period
 #' @param phase_value name of the value of phase_var which you normalizes towards, defaults to Baseline
@@ -20,26 +21,32 @@
 #'
 #' d %>%
 #'   format_normalize(variable = bodytemperature,
+#'                    ID_var = Id,
 #'                    phase_var = Experiment_period,
 #'                    phase_value = Baseline,
 #'                    add = 0)
 #'
 format_normalize <- function(.data,
+                             ID_var,
                              variable,
                              phase_var = Experiment_period,
                              phase_value = Baseline,
                              add = 0){
 
+  ID_var <- rlang::enquo(ID_var)
   variable <- rlang::enquo(variable)
   phase_var <- rlang::enquo(phase_var)
   phase_value <- rlang::enquo(phase_value)
 
-  periodmean <- .data %>%
-    dplyr::filter(!!phase_var == rlang::quo_text(phase_value)) %>%
-    dplyr::pull(!!variable) %>%
-    mean(., na.rm = T)
-
   .data %>%
+    dplyr::group_by(!!ID_var) %>%
+    tidyr::nest() %>%
+    dplyr::mutate(periodmean = purrr::map_dbl(data,
+                                              . %>%
+                                                dplyr::filter(!!phase_var == rlang::quo_text(phase_value)) %>%
+                                                dplyr::pull(!!variable) %>%
+                                                mean(., na.rm = T))) %>%
+    tidyr::unnest() %>%
     dplyr::mutate(!!variable := !!variable - periodmean)
 
 }
